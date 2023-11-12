8c15f8c224fe9debf505a0d0fee73466465cb70d
Daria Koshubarova <koshubarova.da@gmail.com>
2023-11-12 20:02:46 +0600

    1

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,42 +8,56 @@ import Login from '../Login/Login';
 import Register from '../Register/Register';
 import Error from '../Error/Error'
 import Profile from '../Profile/Profile';
-import SearchForm from '../SearchForm/SearchForm';
-import MoviesCardList from '../MoviesCardList/MoviesCardList';
-import { movies, savedMovies } from '../../utils/constants'
-import { useEffect, useState } from 'react';
+import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies'
+// import { movies, savedMovies } from '../../utils/constants'
+// import { useEffect, useState } from 'react';
 
-export default function Main({ name, setLoggedIn }) {
-  const [moviesAll, setMoviesAll] = useState([])
-  const [saveMovie, setSaveMovie] = useState([])
-  const [isCheckMoviesAll, setIsCheckMoviesAll] = useState(true)
-  const [isCheckMoviesSave, setIsCheckMoviesSave] = useState(true)
+export default function Main({ 
+  name, 
+  onRegister, 
+  onLogin, 
+  logOut,
+  editUserData,
+  // setIsError,
+  savedMovies,
+  onDelete,
+  addMovie,
+  // isSuccess,
+  // setSuccess,
+  setIsEdit,
+  isEdit
+}) {
+  // const [moviesAll, setMoviesAll] = useState([])
+  // const [saveMovie, setSaveMovie] = useState([])
+  // const [isCheckMoviesAll, setIsCheckMoviesAll] = useState(true)
+  // const [isCheckMoviesSave, setIsCheckMoviesSave] = useState(true)
 
-  useEffect(() => {
-    setMoviesAll(movies)
-    setSaveMovie(savedMovies)
-  }, [])
+  // useEffect(() => {
+  //   setMoviesAll(movies)
+  //   setSaveMovie(savedMovies)
+  // }, [])
 
-  function onCheckMoviesAll() {
-    if (isCheckMoviesAll) {
-      setIsCheckMoviesAll(false)
+  // function onCheckMoviesAll() {
+  //   if (isCheckMoviesAll) {
+  //     setIsCheckMoviesAll(false)
 
-      setMoviesAll(moviesAll.filter((element) => element.duration <= 40))
-    } else {
-      setIsCheckMoviesAll(true)
-      setMoviesAll(movies)
-    }
-  }
+  //     setMoviesAll(moviesAll.filter((element) => element.duration <= 40))
+  //   } else {
+  //     setIsCheckMoviesAll(true)
+  //     setMoviesAll(movies)
+  //   }
+  // }
 
-  function onCheckMoviesSave() {
-    if (isCheckMoviesSave) {
-      setIsCheckMoviesSave(false)
-      setSaveMovie(saveMovie.filter((element) => element.duration <= 40))
-    } else {
-      setIsCheckMoviesSave(true)
-      setSaveMovie(savedMovies)
-    }
-  }
+  // function onCheckMoviesSave() {
+  //   if (isCheckMoviesSave) {
+  //     setIsCheckMoviesSave(false)
+  //     setSaveMovie(saveMovie.filter((element) => element.duration <= 40))
+  //   } else {
+  //     setIsCheckMoviesSave(true)
+  //     setSaveMovie(savedMovies)
+  //   }
+  // }
 
   return (
     <main className="main">
@@ -56,20 +70,27 @@ export default function Main({ name, setLoggedIn }) {
             <AboutMe />
             <Portfolio />
           </>,
-        signin: <Login name={name} setLoggedIn={setLoggedIn} />,
-        signup: <Register name={name} setLoggedIn={setLoggedIn} />,
+        signin: <Login name={name} onLogin={onLogin} />,
+        signup: <Register name={name} onRegister={onRegister} />,
         error: <Error />,
-        profile: <Profile name={name} setLoggedIn={setLoggedIn} />,
+        profile: <Profile 
+          name={name}
+          logOut={logOut}
+          editUserData={editUserData}
+          // setIsError={setIsError}
+          // isSuccess={isSuccess}
+          // setSuccess={setSuccess}
+          setIsEdit={setIsEdit}
+          isEdit={isEdit} 
+          />,
         movies:
-          <>
-            <SearchForm isCheck={isCheckMoviesAll} changeShot={onCheckMoviesAll} />
-            <MoviesCardList movies={moviesAll} />
-          </>,
+        <>
+        <Movies savedMovies={savedMovies} addMovie={addMovie} />
+      </>,
         savedmovies:
-          <>
-            <SearchForm isCheck={isCheckMoviesSave} changeShot={onCheckMoviesSave} />
-            <MoviesCardList movies={saveMovie} />
-          </>
+        <>
+        <SavedMovies savedMovie={savedMovies} onDelete={onDelete} />
+      </>
       }[name]}
     </main>
   )
